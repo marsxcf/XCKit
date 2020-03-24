27e70714613faea70fdc8fcf62370f7d26265ffa
Xingchen Fang <mars.x.c.f@gmail.com>
2020-03-24 18:11:57 +0800

    init

---
+++ XCKit/UI/XCViewController.swift
@@ -0,0 +1,30 @@
+//
+//  XCViewController.swift
+//  XCKit
+//
+//  Created by Xingchen Fang on 2020/3/24.
+//  Copyright © 2020 Xingchen Fang. All rights reserved.
+//
+
+import UIKit
+
+class XCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ XCKit/XCKit.h
@@ -0,0 +1,19 @@
+//
+//  XCKit.h
+//  XCKit
+//
+//  Created by Xingchen Fang on 2020/3/24.
+//  Copyright © 2020 Xingchen Fang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XCKit.
+FOUNDATION_EXPORT double XCKitVersionNumber;
+
+//! Project version string for XCKit.
+FOUNDATION_EXPORT const unsigned char XCKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XCKit/PublicHeader.h>
+
+
